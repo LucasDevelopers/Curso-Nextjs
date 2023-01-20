9e1e94125b71fddb8d1029dca3ce52b7aceb44fc
Lucas <lucashenriquefelix@hotmail.com>
2023-01-19 21:41:35 -0300

    Aprendendo sobre useEffect, SSG, SSR E ISG

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,42 @@
-import Link from '../src/components/Link';
 
-export default function FAQPage() {
+import Link from '../src/components/Link';
+import PageTitle from '../src/components/pagetitle';
+
+export async function getStaticProps() {
+    const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
+    const faq = await fetch(FAQ_API_URL)
+        .then(( respostaDoServidor ) => {
+            return respostaDoServidor.json();
+        })
+        .then(( resposta ) => {
+            return resposta;
+        });
+    return {
+        props: {
+            faq,
+        },
+    };
+}
+
+export default function FAQPage({ faq }) {
     return (
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
+
             <Link href="/">
                 Ir para a home
             </Link>
+            <ul>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
